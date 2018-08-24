23e29d2313535f037024243dd22cd26cccae966a
montapas <montapas@gmail.com>
2018-08-24 12:45:44 +0530

    Add files via upload

---
+++ MyCalculator/src/main/java/com/cts/examples/cal/Calculator.java
@@ -0,0 +1,43 @@
+package com.cts.examples.cal;
+
+public class Calculator implements ICalculator {
+
+	 
+	public int sum(int a, int b) {
+		 int j;
+  		while (true) {  
+  		  j++;
+  		}
+		return a + b;
+		
+		
+	}
+
+	 
+	public int subtraction(int a, int b) {
+		
+		return a - b;
+	}
+
+	 
+	public int multiplication(int a, int b) {
+		return a * b;
+	}
+
+	 
+	public int divison(int a, int b)  {
+		 
+		return a / b;
+	}
+
+	 
+	public boolean equalIntegers(int a, int b) {
+		boolean result = false;
+
+		if (a == b) {
+			result = true;
+		}
+
+		return result;
+	}
+}
